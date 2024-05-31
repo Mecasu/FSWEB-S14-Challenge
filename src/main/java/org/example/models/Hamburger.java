4d52cfbd6286c4c86630c87051a1e23445781405
Mecasu <melihcansunay@live.com>
2024-05-31 10:33:10 +0300

    wip

---
+++ src/main/java/org/example/models/Hamburger.java
@@ -0,0 +1,52 @@
+package org.example.models;
+
+public class Hamburger {
+
+    private String name;
+    private String meat;
+    private String breadRollType;
+    private double price;
+
+    private String addition1Name;
+    private double addition1Price;
+    private String addition2Name;
+    private double addition2Price;
+    private String addition3Name;
+    private double addition3Price;
+    private String addition4Name;
+    private double addition4Price;
+
+    public Hamburger(String name, String meat, String breadRollType, double price) {
+        this.name = name;
+        this.meat = meat;
+        this.breadRollType = breadRollType;
+        this.price = price;
+    }
+
+    public void addHamburgerAddition1(String addition1Name, double addition1Price) {
+        this.addition1Name = addition1Name;
+        this.addition1Price = addition1Price;
+    }
+
+    public void addHamburgerAddition2(String addition2Name, double addition2Price) {
+        this.addition2Name = addition2Name;
+        this.addition2Price = addition2Price;
+    }
+
+    public void addHamburgerAddition3(String addition3Name, double addition3Price) {
+        this.addition3Name = addition3Name;
+        this.addition3Price = addition3Price;
+    }
+
+    public void addHamburgerAddition4(String addition4Name, double addition4Price) {
+        this.addition4Name = addition4Name;
+        this.addition4Price = addition4Price;
+    }
+
+    public double itemizeHamburger() {
+        double totalPrice = price + addition1Price + addition2Price + addition3Price + addition4Price;
+        System.out.println("Name: " + name + "\nMeat: " + meat + "\nBreadRollType: " + breadRollType + "\nAddition1: " + addition1Name + "\nAddition2: " + addition2Name + "\nAddition3: " + addition3Name + "\nAddition4: " + addition4Name);
+        System.out.println("Toplam: " + totalPrice);
+        return totalPrice;
+    }
+}
